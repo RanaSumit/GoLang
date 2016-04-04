447da04949b19c7f3edfa51001b1212e6933bb1d
RanaSumit <rana91sumit@gmail.com>
2016-04-03 22:24:24 -0700

    Updated

---
+++ FindFirstK/firstk.go
@@ -0,0 +1,27 @@
+package bstrees
+
+// FindFirstK returns the first occurrence of k in tree
+// (when encountering in-order) or nil if k isn't present.
+// The time complexity is O(h) where h is the tree height.
+// The O(1) additional space is needed.
+type BSTree struct {
+	Data  interface{}
+	left  *BSTree
+	right *BSTree
+}
+
+func FindFirstK(tree *BSTree, k int) *BSTree {
+	var c *BSTree
+	node := tree
+	for node != nil {
+		switch v := node.Data.(int); {
+		case v > k:
+			node = node.left
+		case v == k:
+			c, node = node, node.left
+		case v < k:
+			node = node.right
+		}
+	}
+	return c
+}
